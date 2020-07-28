37adaeb309397fae33e6326809ba38d87f01ef78
KillerCode_236 <45013989+Rubencrash236@users.noreply.github.com>
2020-07-27 21:13:45 -0400

    newvas cositas

--- Doble Spooler de Impresora/Printer.cs
+++ Doble Spooler de Impresora/Printer.cs
@@ -1,12 +1,30 @@
-using System;
+﻿using System;
 using System.Threading;
 
-public class Printer
+namespace Doble_Spooler_de_Impresora
 {
-    public Printer()
+    public class Printer
     {
-        bool ready;
-        int type;
-        Thread print;
+        public bool ready { get; set; }
+        public int id { get; set; }
+        public int type { get; set; }
+        public Thread print { get; set; }
+        public Work myWork { get; set; }
+        public Printer(int id, int type)
+        {
+            this.ready = true;
+            this.id = id;
+            this.type = type;
+            print = new Thread(startPrint);
+        }
+
+        public void startPrint()
+        {   
+            this.ready = false;
+            Console.WriteLine(this.myWork.work);
+            this.ready = true;
+            print.Abort();
+        }
+
     }
 }
--- Doble Spooler de Impresora/Program9.cs
+++ Doble Spooler de Impresora/Program9.cs
@@ -1,79 +1,57 @@
 ﻿using System;
 using System.Threading;
 using System.Collections;
+using static Doble_Spooler_de_Impresora.Work;
+using static Doble_Spooler_de_Impresora.Printer;
+
+
 
 
 namespace Doble_Spooler_de_Impresora
 {
     class Program
     {
-        static Stack primary;
-        static Stack secundary;
-
+        static Printer[] printers;
         static void Main(string[] args)
         {
-            primary = new Stack();
-            secundary = new Stack();
+
+            Stack[] works = new Stack[3]; 
             Console.WriteLine("Cantidad de impresoras tipo A: ");
-            int typeA = Console.ReadLine();
+            int typeA = Int32.Parse(Console.ReadLine());
             Console.WriteLine("Cantidad de impresoras tipo B: ");
-            int typeB = Console.ReadLine();
-            Thread picker = new Thread(pick);
-            Thread[] Pspooler = new Thread[sizeA](imprimir_A);
-            Thread[] Sspooler = new Thread[sizeB](imprimir_B);
+            int typeB = Int32.Parse(Console.ReadLine());
+            printers = new Printer[typeA + typeB];
 
-            Pspooler.Start();
-            Sspooler.Start();
+            //works[0].push(elemento); <-- para el tipo a :v y ya tu sabe pa losotro ;3
+
+            for(int i = 0; i < typeA; i++)
+            {
+                printers[i] = new Printer(i, 1);
+            }
+            for(int i= typeA - 1; i < printers.Length; i++)
+            {
+                printers[i] = new Printer(i, 2);
+            }
         }
 
-        static void imprimir_A()
+        static void searchPrinter(Work myWork)
         {
-            while (true)
-            {
-                if(primary.Count == 10)
-                {
-                    Thread.Sleep(2000);
-                }
-                else if(primary.Count < 10)
-                {
-                    primary.Add(Console.ReadKey().KeyChar);
-                    Thread.Sleep(500);
-                }
-
-                Console.Write("spooler 1: " + primary.Pop().ToString() + " \n");
-                
-                Thread.Sleep(500);
-            }
             
-        }
-
-        static void imprimir_B()
-        {
             while (true)
             {
-                Thread.Sleep(500);
-                secundary.Add(Console.ReadKey().KeyChar);
-                if (secundary.Count > 0)
+                foreach (Printer printer in printers)
                 {
-                    Console.Write("spooler 2:" + secundary[0] + " \n");
-                    secundary.RemoveAt(0);
+                    if (printer.ready && (printer.type == myWork.type || myWork.type == 3))
+                    {
+                        printer.myWork = myWork;
+                        printer.print.Start();
+                       
+                    }
                 }
-
-                Thread.Sleep(1000);
             }
         }
 
-        static void printing()
-        {
 
-        }
 
-        static void pick()
-        {
-            Console.WriteLine();
-            primary.Push(Console.ReadKey().KeyChar);
-            Console.Clear();
-            Thread.Sleep(10000);
-        }
     }
 }
